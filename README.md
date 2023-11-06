6134f5604bca13b161a7704a0af5d3bf5701a614
Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-11-06 10:51:05 +0100

    chore: few non-functional updates (#2)

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 web3-plugin-template
 ===========
 
+This is a template for creating a repository for web3.js plugin.
+
+How to use
+------------
+
+1. Create your project out of this template.
+
+    You can do so by pressing on `Use this template` on the above right corner and then select `Create new Repositor`. Please, use the convention `web3-plugin-<name>` for your repo name.
+2. Update the `name` and `description` fileds at your `package.json`.
+
+    Chose a name like: `@<organization>/web3-plugin-<name>` (or the less better `web3-plugin-<name>`).
+3. Update the code inside `src` folder.
+
+4. Modify and add tests inside `test` folder.
+
+5. Publish to the npm registry.
+
+    You can publish with something like: `yarn build && npm publish --access public`.
+
 Contributing
 ------------
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,8 @@
-import Web3, { core } from "web3";
+import { Web3, core } from "web3";
 import { TemplatePlugin } from "../src";
 
 describe("TemplatePlugin Tests", () => {
-  it("should register TokensPlugin plugin on Web3Context instance", () => {
+  it("should register TemplatePlugin plugin on Web3Context instance", () => {
     const web3Context = new core.Web3Context("http://127.0.0.1:8545");
     web3Context.registerPlugin(new TemplatePlugin());
     expect(web3Context.template).toBeDefined();
@@ -11,11 +11,11 @@ describe("TemplatePlugin Tests", () => {
   describe("TemplatePlugin method tests", () => {
     let consoleSpy: jest.SpiedFunction<typeof global.console.log>;
 
-    let web3Context: Web3;
+    let web3: Web3;
 
     beforeAll(() => {
-      web3Context = new Web3("http://127.0.0.1:8545");
-      web3Context.registerPlugin(new TemplatePlugin());
+      web3 = new Web3("http://127.0.0.1:8545");
+      web3.registerPlugin(new TemplatePlugin());
       consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
     });
 
@@ -24,7 +24,7 @@ describe("TemplatePlugin Tests", () => {
     });
 
     it("should call TempltyPlugin test method with expected param", () => {
-      web3Context.template.test("test-param");
+      web3.template.test("test-param");
       expect(consoleSpy).toHaveBeenCalledWith("test-param");
     });
   });
